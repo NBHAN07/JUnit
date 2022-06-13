22c5a21dbbc025ad2026a70b817d305398869f1c
NBHAN07 <104321022+NBHAN07@users.noreply.github.com>
2022-06-14 00:13:19 +0300

    Update Deneme2.java
    14.06.2022

--- src/test/java/day15_writeExcel_screenShot/Deneme2.java
+++ src/test/java/day15_writeExcel_screenShot/Deneme2.java
@@ -2,6 +2,6 @@ package day15_writeExcel_screenShot;
 
 public class Deneme2 {
     public static void main(String[] args) {
-        System.out.println("bugun bu is burada bitecek!");
+        System.out.println("bugun bu is burada bitecek! guncelleme: bitti!");
     }
 }
